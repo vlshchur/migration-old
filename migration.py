fc9cab66604d6d49c511586d26cb48afb3615fba
Vladimir Shchur <vlshchur@gmail.com>
2017-12-20 18:12:41 -0800

    JAFS computation, one population, restructuring

--- migration.py
+++ migration.py
@@ -8,32 +8,19 @@ import math
 from math import (exp,log)
 
 from CorrectLambda import CorrectLambda
-
-#lineage = collections.namedtuple('lineage', ['d0', 'd1', 'pop' ])#d0: number of descendents in population 0; d1: number of descendents in population 1; pop: current population of lineage
-
-class lineage:
-    def __init__(self, d0, d1, pop, fc = -1):
-        self.d0 = d0
-        self.d1 = d1
-        self.pop = pop
-        self.fc = fc
-
-#class lineage(namedtuple('lineage', ['d0', 'd1', 'pop' , 'fc']):#d0: number of descendents in population 0; d1: number of descendents in population 1; pop: current population of lineage; fc - for lineage with three decendents is the sum of descendent populations in first coalescence
-#    def __new__(cls, d0, d1, pop, fc=0):
-#        return super(TemplateContainer, cls).__new__(cls, d0, d1, pop, fc)
+from TwoPopulations import TwoPopulations
+from OnePopulation import OnePopulation
 
 class MigrationInference:
     def __init__(self):
+        self.theta = 0.00001#coalescent mutation rate theta/2
         self.splitT = 3
         self.mu = [0.00001,0.00001]
-        self.la = [0.1, 0.01]
-        self.Msize = 52
-        self.Pa = [0 for i in range(self.Msize)]
-        self.Pa[2] = 1
-#        self.P0 = mat(self.P0)
-        self.P1 = [0 for i in range(self.Msize)]
-#        self.P1 = mat([0 for i in range(self.Msize)])
-        self.Pexpected = [0 for i in range(self.Msize)]
+        self.M = None
+        self.integralP = None
+        self.P0 = None
+        self.P1 = None
+        self.JAFS = [0 for i in range(7)]#0100,1100,0001,0101,1101,0011,0111
 #        self.expP = mat([0 for i in range(self.Msize)])
         #self.expectP = [[0 for i in range(self.Msize)] for j in range(self.numT)]
         #PSMC parameters
@@ -44,178 +31,25 @@ class MigrationInference:
         #self.M - transition matrix
         #self.Minv - inversed transition matrix
         #self.Mexp - exponentiated matrix
-        
-    def CheckState(self, state):
-        tn0, tn1 = 0, 0
-        state[:] = sorted(state, key=lambda line: line.pop)
-        state[:] = sorted(state, key=lambda line: line.d0, reverse = True)
-        state[:] = sorted(state, key=lambda line: line.d0+line.d1, reverse=True)
-        for val in state:
-            tn0 += val.d0
-            tn1 += val.d1
-        if tn0 != 2 or tn1 != 2:
-            print("CheckState() not passed: expected number of lineages is 2 and 2, instead there are", tn0, "and", tn1, "lineages")
-            sys.exit(0)
-        if len(state) == 2 and state[0].d0 + state[0].d1 == 3 and state[0].fc == -1:
-            st = self.PrintState(state)
-            print("CheckState() not passed: first coalescence for lineage with three descendants is not set:", st)
-            sys.exit(0)
     
-    def MapStateToInd(self, state):
-        self.CheckState(state)
-        if len(state) == 4:
-            i, j = 0, 0
-            for val in state:
-                if val.d0 == 0:
-                    i += val.pop
-                else:
-                    j += val.pop
-            return i+3*j
-        if len(state) == 3:
-            if state[0].d0 + state[0].d1 != 2:
-                print("Unexpected number of lineages, expected 2, recieved", state[0].d0 + state[0].d1)
-                sys.exit(0)
-            if state[0].d0 == 2:
-                return 9 + 3*state[0].pop + state[1].pop + state[2].pop
-            elif state[0].d0 == 1:
-                return 9 + 6 + 4*state[0].pop + 2*state[1].pop + state[2].pop
-            elif state[0].d0 == 0:
-                return 9 + 6 + 8 + 3*state[0].pop + state[1].pop + state[2].pop
-        if len(state) == 2:
-            if state[0].d0 == 2 and state[0].d1 == 1:
-                return 29 + 2*state[0].pop + state[1].pop + 4 * state[0].fc
-            elif state[0].d0 == 1 and state[0].d1 == 2:
-                return 29 + 8 + 2*state[0].pop + state[1].pop + 4 * (state[0].fc - 1)
-            elif state[0].d0 == 2 and state[0].d1 == 0:
-                return 29 + 8 + 8 + 2*state[0].pop + state[1].pop
-            elif state[0].d0 == 1 and state[0].d1 == 1:
-                return 29 + 8 + 8 + 4 + state[0].pop + state[1].pop
-        return self.Msize
-    
-    def MapIndToState(self, ind):
-        if not isinstance(ind, int) or ind < 0 or ind >= self.Msize:
-            print("Unexpected index value", ind, ", index should be an integer between 0 and", self.Msize, ".")
-            sys.exit(0)
-        if ind < 9:
-            state = [lineage(1, 0, 0), lineage(1, 0, 0), lineage(0, 1, 0), lineage(0, 1, 0)]
-            i = ind//3
-            j = ind%3
-            while i > 0:
-                self.UpdateLineagePop(state, i-1, 1)
-                i -= 1
-            while j > 0:
-                self.UpdateLineagePop(state, 2+j-1, 1)
-                j -= 1
-        elif ind < 29:
-            if ind < 15:
-                state = [lineage(2, 0, 0), lineage(0, 1, 0), lineage(0, 1, 0)]
-                self.UpdateLineagePop(state, 0, (ind - 9)//3)
-                i = (ind - 9)%3
-                while i > 0:
-                    self.UpdateLineagePop(state, i, 1)
-                    i -= 1
-            elif ind < 23:
-                state = [lineage(1, 1, 0), lineage(1, 0, 0), lineage(0, 1, 0)]
-                self.UpdateLineagePop(state, 0, (ind - 15)//4)
-                self.UpdateLineagePop(state, 1, ((ind - 15)%4)//2 )
-                self.UpdateLineagePop(state, 2, ((ind - 15)%4)%2 )
-            else:
-                state = [lineage(0, 2, 0), lineage(1, 0, 0), lineage(1, 0, 0)]
-                self.UpdateLineagePop(state, 0, (ind - 23)//3)
-                i = (ind - 23)%3
-                while i > 0:
-                    self.UpdateLineagePop(state, i, 1)
-                    i -= 1
-        else:
-            if ind < 37:
-                state = [lineage(2, 1, 0), lineage(0, 1, 0)]
-                popTmp = (ind - 29)%4
-                self.UpdateLineagePop(state, 0, popTmp//2)
-                self.UpdateLineagePop(state, 1, popTmp%2)
-                self.UpdateLineageFC(state, 0, (ind - 29)//4)
-            elif ind < 45:
-                state = [lineage(1, 2, 0), lineage(1, 0, 0)]
-                popTmp = (ind - 37)%4
-                self.UpdateLineagePop(state, 0, popTmp//2)
-                self.UpdateLineagePop(state, 1, popTmp%2)
-                self.UpdateLineageFC(state, 0, (ind - 37)//4 + 1)
-            elif ind < 49:
-                state = [lineage(2, 0, 0), lineage(0, 2, 0)]
-                self.UpdateLineagePop(state, 0, (ind - 45)//2)
-                self.UpdateLineagePop(state, 1, (ind - 45)%2)
-            else:
-                state = [lineage(1, 1, 0), lineage(1, 1, 0)]
-                if ind == 51:
-                    self.UpdateLineagePop(state, 0, 1)
-                if ind == 50 or ind == 51:
-                    self.UpdateLineagePop(state, 1, 1)
-        self.CheckState(state)
-        return state
-    
-    def UpdateLineagePop(self, state, ind, pop):
-        l = lineage(state[ind].d0, state[ind].d1, pop, state[ind].fc)
-        state[ind] = l
-    
-    def UpdateLineageFC(self, state, ind, fc):
-        l = lineage(state[ind].d0, state[ind].d1, state[ind].pop, fc)
-        state[ind] = l
-    
-    def PrintState(self, state):
-        st = ""
-        for val in state:
-            if val.d0 + val.d1 != 3:
-                st = st + "(" + str(val.d0) + "," + str(val.d1) + "," + str(val.pop) + ") "
-            else:
-                if val.fc == 0:
-                    fc = "0+0"
-                elif val.fc == 1:
-                    fc = "0+1"
-                elif val.fc == 2:
-                    fc = "1+1"
-                else:
-                    fc = str(val.fc) + " UNEXPECTED VALUE"
-                st = st + "(" + str(val.d0) + "," + str(val.d1) + "," + str(val.pop) + "," + str(fc) + ") "
-        return st
-        
-    def SetMatrix(self):
-        self.MM = [[0 for col in range(self.Msize)] for row in range(self.Msize)]#transition matrix
-        self.CM = [[0 for col in range(self.Msize)] for row in range(self.Msize)]#coalescence matrix
+    def PrintMatrix(self):
         for i in range(self.Msize):
-            self.UpdateMatrixRow(i)
-        self.MM = mat(self.MM)
-        self.CM = mat(self.CM)
-#        print self.MM
-        
-    def UpdateMatrixRow(self, ind):
-        state = self.MapIndToState(ind)
-        total = 0
-        for i in range(len(state)):
-            #Migration
-            state1 = list(state)
-            self.UpdateLineagePop(state1, i, (state[i].pop+1)%2)
-            ind2 = self.MapStateToInd(state1)
-            self.MM[ind][ind2] += self.mu[ state[i].pop ]
-            total += self.mu[ state[i].pop ]
-            #Coalescence
-            for j in range(len(state)):
-                if j == i or state[j].pop != state[i].pop:
-                    continue
-                state1 = list(state)
-                for k in sorted([i,j], reverse=True):
-                    del state1[k]
-                line = lineage(state[i].d0+state[j].d0, state[i].d1+state[j].d1, state[i].pop)
-                if line.d0 + line.d1 == 3:
-                    if state[i].d0+state[i].d1 == 2:
-                        line.fc = state[i].d1
-                    else:
-                        line.fc = state[j].d1
-                state1.append(line)
-                ind2 = self.MapStateToInd(state1)
-                if ind2 != self.Msize:
-                    self.MM[ind][ind2] += self.la[ state[i].pop ]
-                    self.CM[ind][ind2] += self.la[ state[i].pop ]
-                total += self.la[ state[i].pop ]/2.0
-        self.MM[ind][ind] -= total
+            for j in range(self.Msize):
+                el = format(self.MM.item(i,j), '.10g')
+                print( el, end = "\t" )
+            print("")
+            
+    def PrintMatrixRow(self, rn):
+        st = self.MapIndToState(rn)
+        print (self.PrintState(st) )
+        for i in range(self.Msize):
+            el = format(self.MM.item(rn, i), '.10g')
+            print( el, end = "\t" )
+        print("")
+        for i in range(self.Msize):
+            if self.MM.item(rn, i) != 0:
+                st = self.MapIndToState(i)
+                print (self.PrintState(st) )
         
     def CorrectLambdas(self):
         p0 = [[1,0,0],[0,1,0]]
@@ -236,29 +70,48 @@ class MigrationInference:
         return True
             
     def JAFSpectrum(self):
-        self.SetMatrix()
-        print(self.MM)
-        a = 1
+        model = TwoPopulations(self.lc[0][0], self.lc[0][1], self.mu[0], self.mu[1])
+        self.P0 = [0.0 for i in range(model.Msize)]
+        self.P0[2] = 1.0
+        for interval in range(self.numT):
+            print("Interval", interval)
+            if interval < self.splitT:
+                model = TwoPopulations(self.lc[interval][0], self.lc[interval][1], self.mu[0], self.mu[1])
+            else:
+                model = OnePopulation(self.lc[interval][0])
+            if interval == self.splitT:
+                self.CollapsePops()
+            self.M = model.SetMatrix()
+            print(self.M)
+            self.SolveDifEq(interval)
+            for i in range(model.Msize):
+                jaf = model.StateToJAF(i)
+                self.JAFS = [x + y*self.integralP[i] for x,y in zip(self.JAFS, jaf)]
+            self.P0 = self.P1
     
-    def ExpectationP(self, interval):
-        if interval == 0:
-            a = 0
+    def CollapsePops(self):
+        Pc = [0 for i in range(8)]
+        Pc[0] = sum(self.P0[0:9])
+        Pc[1] = sum(self.P0[9:15])
+        Pc[2] = sum(self.P0[15:23])
+        Pc[3] = sum(self.P0[23:29])
+        Pc[4] = sum(self.P0[29:33])
+        Pc[5] = sum(self.P0[33:37])
+        Pc[6] = sum(self.P0[37:41])
+        Pc[7] = sum(self.P0[41:44])
+        self.P0 = Pc
+    
+    def SolveDifEq(self, interval):
+        if interval < self.numT - 1:
+            T = self.times[interval]
+            MET = linalg.expm( dot(self.M,T) )
+            self.P1 = dot(MET,self.P0)
         else:
-            a = sum(self.times[0:interval-1])
-        
-        b = a + times[interval]
-        T = b - a
-        if interval == self.numT:
-            MMET = [[0 for col in range(self.Msize)] for row in range(self.Msize)]#transition matrix
-        else:
-            MMET = linalg.expm( dot(self.M,T) )
-        MMI = linalg.inv(self.MM)
-        Pb = dot(MMET,self.Pa)
-        Pe = [x - y for x, y in zip(Pb, Pa)]
-        Pe = dot(MMI, Pe)
-        Pe = [b*x - a*y  - z for x, y, z in zip(Pb, Pa, Pe)]
-        Pe = dot(MMI,Pe)
-        self.Pexpected = [x + y for x, y in zip(self.Pexpected, Pe)]
+            self.P1 = [0 for i in range( len(self.P0) )]
+        MI = linalg.inv(self.M)
+        sizeM = self.M.shape[0]
+        self.integralP = [x - y for x, y in zip(self.P1, self.P0)]
+        self.integralP = dot(MI,self.integralP)
     
     def ObjectiveFunction(self, mu):
         self.cl.SetMu(mu[0], mu[1])
@@ -267,6 +120,10 @@ class MigrationInference:
             return -10**(100)
         print(self.lc)
         self.JAFSpectrum()
+        norm = sum(self.JAFS)
+        print("----------",self.JAFS[0]/norm,self.JAFS[1]/norm,sep="\t\t")
+        print(self.JAFS[2]/norm,self.JAFS[3]/norm,self.JAFS[4]/norm,sep="\t\t")
+        print(self.JAFS[5]/norm,self.JAFS[6]/norm,"----------",sep="\t\t")
 #        return self.Likelihood()
         return log(1)
     
@@ -277,20 +134,7 @@ class MigrationInference:
 #        optimize ObjectiveFunction(mu0, mu1)
     
     def Test(self):
-        if 0:#Test if index-state-index mapping is identity
-            for i in range(self.Msize):
-                st = self.MapIndToState(i)
-                print( self.MapStateToInd(st) )
         self.Solve()
-    
-    def Test1(self):
-        self.SetMatrix()
-        #self.MM = matrix(self.MM)
-        m = []
-        for i in range(9):
-            m.append(self.MM[i][0:9])
-        m = matrix(m)
-        print(m)
         
 Migration = MigrationInference()
 Migration.Test()
